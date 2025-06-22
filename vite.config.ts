cea6afbabb214944b4e09f9f55093433d04f3ced
kevin-coscarelli <kevinsl1991@gmail.com>
2025-06-22 12:08:04 +0200

    - Missing vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/post-list/',
+    base: '/posts-list/',
     plugins: [vue(), tailwindcss()],
 })
